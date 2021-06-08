ae102b13eec025fcd56b0ba6775a72a06379b276
psb-david-petty <psb-david-petty@users.noreply.github.com>
2021-06-08 12:04:27 -0400

    Initial commit of new .PY files and new images.

---
+++ src/files.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# files.py
+#
+import os
+"""
+Collect all the filenames of card faces.
+"""
+1234567890123456789012345678901234567890123456789012345678901234567890
+# Use os.walk to collect all the image files.
---
+++ src/unittests.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#
+# unittests.py
+#
+import unittest
+import layout
+class TestLayoutFunction(unittest.TestCase):
+
+    tests = [
+        (2, (2, 1, ), ),
+        (32, (8, 4, ), ),
+        (106, (53, 2, ), ),
+        (64, (8, 8, ), ),
+    ]
+    def test_layouts(self):
+        for n, size in self.tests:
+            self.assertEqual(layout.sizing(n), size)
+
+unittest.main()
