b4b1b1faa2de4350033cb453ac90a2bd699495df
psb-david-petty <psb-david-petty@users.noreply.github.com>
2021-06-11 23:24:47 -0400

    Defined card_pathnames function to encapsulate the os.walk code. Included pathname manipulation using os.path.

--- src/files.py
+++ src/files.py
@@ -2,9 +2,26 @@
 #
 # files.py
 #
-import os
+import os, os.path
 """
 Collect all the filenames of card faces.
 """
 1234567890123456789012345678901234567890123456789012345678901234567890
-# Use os.walk to collect all the image files.
+
+def card_pathnames(path, ext='.png'):
+    """Return paths to files in path with ext."""
+    # Create list for cards.
+    cards = []
+
+    # Iterate through files w/ "png" and append them to cards list.
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            head, tail = os.path.splitext(name)
+            if ext == tail:
+                cards.append(os.path.join(root, name))
+    return cards
+
+cards = card_pathnames('./cards/faces/')
+
+if __name__ == "__main__":
+    print(len(cards), cards)
