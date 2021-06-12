ca379ad8e2073b0cb68f29022b48e4c8f3fd8187
psb-david-petty <psb-david-petty@users.noreply.github.com>
2021-06-11 23:18:24 -0400

    Cleaned up some of app code. Added card faces from pathname list to all cards. Added timer updating.

--- src/app.py
+++ src/app.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 #
-# test.py
+# app.py
 #
 # https://www.pythontutorial.net/tkinter/tkinter-optionmenu/
 #
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-import layout
+import files, layout, gameplay, timer
 
 1234567890123456789012345678901234567890123456789012345678901234567890
 
 _ww, _wh, _pad = 600, 600, 20
 numbers = (6,  8,  9,  10,  12,  14,  15,  18,  20,  21,  24,  27,  28,  32,  35,  36,  40,  44,  45,  50,  )
-number = 12
+number = 6
 color = '#663399'
 
 """
@@ -21,6 +21,7 @@ Create the App.
 """
 class App(tk.Tk):
     # Initialize data.
+    _cards = files.cards
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -43,19 +44,23 @@ class App(tk.Tk):
 
         # Center frame.
         frm = ttk.Frame(self, width=600, height=600, padding=20)
-        frm.grid(row=0, column=0, sticky='')
+        frm.grid(row=0, column=0, sticky=None)
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
         # Create info.
         info = Info(frm)
-        info.pack(fill=tk.X)
+        info.pack(fill=tk.X, expand=True)
         self._info = info
 
         # Create game.
         game = Game(frm)
-        game.pack(fill=tk.X)
+        game.pack(fill=None, expand=True)
         self._game = game
+
+        # Start timer.
+        timer.echo = self._info.update_time
+        timer.timer.start()
 
         self.update_and_print()
 
@@ -66,6 +71,7 @@ class App(tk.Tk):
         print(f"ix={self._info.winfo_width()}; iy={self._info.winfo_height()};")
         print(f"gx={self._game.winfo_width()}; gy={self._game.winfo_height()};")
         self.minsize(width=600+20+20, height=600+20+20+self._info.winfo_height())
+
 
 class Info(ttk.Frame):
 
@@ -79,12 +85,6 @@ class Info(ttk.Frame):
         self.create_widgets(number, numbers)
 
     def create_widgets(self, number, numbers):
-        """
-        # Get frame attributes.
-        fw, fh, pad = (int(tcl_obj.string)
-            for tcl_obj in (self['width'], self['height'], self['padding'][0], ))
-        print(fw, fh, pad)
-        """
         # padding for widgets using the grid layout
         paddings = {'padx': 5, 'pady': 5}
 
@@ -111,23 +111,27 @@ class Info(ttk.Frame):
         self.labels.grid(row=2, column=0, sticky='', **paddings)
 
         # timer label
-        self.labelt = ttk.Label(self, foreground='green', text="01:00")
+        self.labelt = ttk.Label(self, foreground='red', text="00:00")
         self.labelt.grid(row=2, column=1, sticky=tk.E, **paddings)
 
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
 
     def option_changed(self, *args):
+        timer.reset()
         number = int(self._option.get()) * 2
         self.labels['text'] = f'You selected: {layout.sizing(number)}'
         self.master.master._game.create_widgets(number)
         self.master.master.update_and_print()
 
+    def update_time(self, str):
+        self.labelt.configure(text=str)
+
 class Game(ttk.Frame):
     
     def __init__(self, parent, **kwargs):
         super().__init__(parent, **kwargs)
-        self.create_widgets(number)
+        self.create_widgets(number * 2)
 
     def on_click(self, button):
         info = button.grid_info()
@@ -139,17 +143,12 @@ class Game(ttk.Frame):
           widgets.destroy()
 
     def create_widgets(self, number):
-        """
-        # Get frame attributes.
-        fw, fh, pad = (int(tcl_obj.string)
-            for tcl_obj in (self['width'], self['height'], self['padding'][0], ))
-        """
         # Clear frame.
         self.clear_frame()
 
         # Load an image, resize it, create a PhotoImage.
         # https://stackoverflow.com/questions/52307290/what-is-the-difference-between-images-in-p-and-l-mode-in-pil/52307690#52307690
-        png = Image.open("./cards/blue_back.png").convert('RGB')
+        png = Image.open("./cards/backs/blue_back.png").convert('RGB')
 
         # Calculate the scale.
         gw, gh, pad = 600, 600, 10
@@ -158,22 +157,28 @@ class Game(ttk.Frame):
         w, h = int(png.width / scale - pad), int(png.height / scale - pad)
 
         print(f"Card from: {png.width}x{png.height} to: {w}x{h} is a scale of {scale:.2f}")
-        png = png.resize((w, h,), Image.LANCZOS)
-        self._img = ImageTk.PhotoImage(png)
+        back = png.resize((w, h,), Image.LANCZOS)
+        self._back = ImageTk.PhotoImage(back)
 
         # Set button style
         style = ttk.Style()
         style.theme_use('default')
         style.configure('game.TButton', background=color, borderwidth=1)
 
-        # Add buttons in a grid.
-        self._buttons = ny * [ nx * [None]]
+        # Add images and buttons in a grid.
+        self._images = [[None for i in range(nx)] for j in range(ny)]
+        self._buttons = [[None for i in range(nx)] for j in range(ny)]
         for r in range(ny):
             for c in range(nx):
-                # TODO: not sure why 4 works
-                self._buttons[r][c] = ttk.Button(self, style='game.TButton', image=self._img)
+                # Create scaled PhotoImage.
+                png = Image.open(files.cards[r * nx + c]).convert('RGB')
+                face = png.resize((w, h,), Image.LANCZOS)
+                self._images[r][c] = ImageTk.PhotoImage(face)
+                # Create Button w/ PhotoImage
+                self._buttons[r][c] = ttk.Button(self, style='game.TButton', image=self._images[r][c])
                 self._buttons[r][c].grid(row=r, column=c)
                 self._buttons[r][c].configure(command=lambda button=self._buttons[r][c]: self.on_click(button))
+        del self._buttons
 
 if __name__ == "__main__":
     app = App()
