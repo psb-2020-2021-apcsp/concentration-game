5db538201722225e3b8a059b836531fed70384db
psb-david-petty <psb-david-petty@users.noreply.github.com>
2021-06-27 22:33:19 -0400

    Added logger (currently not used).

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@
 #
 # main.py
 #
-import app, sys
+import app, log, sys
 
 1234567890123456789012345678901234567890123456789012345678901234567890
 """
 Concentration card game. This module currently creates the window and widgets.
 """
+
+logger = log.log(__name__)    # initialize logger
 
 if __name__ == "__main__":
     concentration = app.App()
