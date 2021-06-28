08a54f0ae3480f1a885e0ff3cf99c0551ff09bb0
psb-david-petty <psb-david-petty@users.noreply.github.com>
2021-06-27 22:30:37 -0400

    Renamed start & stop. Removed correct (now in score.py). Cleaned up comments.

--- src/gameplay.py
+++ src/gameplay.py
@@ -12,16 +12,17 @@ Implement the finite state machine for game play.
 logger = log.log(__name__)  # initialize logger
 
 # Initialize module data.
-state, first, second, correct = 0, None, None, list()
+state, first, second = 0, None, None
 # Callbacks for show_back, show_face, get_id, echo w/ defaults.
 show = lambda f, s: logger.info(f"{f}({s})")
-show_back, show_face, get_id = lambda s: show('back', s), lambda s: show('frnt', s), \
-    lambda s: [ f"pyimage{s[0]}", f"pyimage{s[0]}", ][s[1]]
-start, stop = lambda: logger.info(f"start..."), lambda: logger.info(f"stop...")
+delay = lambda s: logger.info(f"{s}")
+show_back, show_face = lambda s: show('back', s), lambda s: show('frnt', s),
+get_id = lambda s: [ f"pyimage{s[0]}", f"pyimage{s[0]}", ][s[1]]
+start_delay, stop_delay = lambda: delay(f"start..."), lambda: delay(f"stop...")
 
 def reset():
-    global state, first, second, correct
-    state, first, second, correct = 0, None, None, list()
+    global state, first, second
+    state, first, second = 0, None, None
 
 def fsm(signal=None):
     """Perform one step of the game-play FSM by processing signal.
@@ -30,7 +31,7 @@ def fsm(signal=None):
     - If signal is None, then it represents a timeout.
     TODO: a bit of a kludge, but workable for one oddball signal."""
     # Remember state and clicked values.
-    global state, first, second, correct
+    global state, first, second
     states = [ 'start', 'clicked', 'wait', ]
     logger.info(f"start: {states[state]} {signal} ({first} {second})")
 
@@ -56,27 +57,27 @@ def fsm(signal=None):
                         f"'{get_id(second)}'")
             if get_id(first) == get_id(second):
                 score.score(get_id(signal))
-                state = 0   # start
-                # Reinitialize first and second just for info logging.
+                # Reinitialize first and second to clarify logging.
                 first, second = None, None
+                state = 0   # start
             else:
                 score.score()
-                start()
+                start_delay()
                 state = 2   # wait
     elif state == 2:    # wait - clicked twice and failed
         if signal is None:
             show_back(first)
             show_back(second)
-            # Reinitialize first and second just for info logging.
+            # Reinitialize first and second to clarify logging.
             first, second = None, None
             state = 0   # start
         if signal is not None and score.matched(get_id(signal)):
             state = 2   # wait
         if signal is not None and not score.matched(get_id(signal)):
-            stop()
+            stop_delay()
             show_back(first)
             show_back(second)
-            # Reinitialize first and second just for info logging.
+            # Reinitialize first and second to clarify logging.
             first, second = None, None
             first = signal
             show_face(signal)
@@ -85,7 +86,6 @@ def fsm(signal=None):
 
 if __name__ == "__main__":
     import time
-
     reset()
 
     # timeout
